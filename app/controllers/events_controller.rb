600ff3ae183a6af722c46a3db0770f9a13ab8336
Jesse Lin <jesselin@gmail.com>
2013-09-21 16:36:11 -0700

    Event and Interest controllers fleshed out

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,58 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save 
+      redirect_to event_path(@event)
+    else
+      render action: 'new'
+    end
   end
 
   def show
+    set_event
   end
 
   def edit
+    set_event
   end
 
   def update
+    set_event
+
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render action: 'edit'
+    end
+
   end
 
   def destroy
+    if set_event.destroy
+      redirect_to events_path, notice: 'Event deleted.'
+    else
+      render action: 'index'
+    end
   end
+
+  private
+    
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit([:name, :description, :start_date, :end_date, :start_time, :end_time, :location, :address, :city, :state, :zip_code, :owner_user_id, :interest_id])
+    end
+
 end
--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,22 +1,58 @@
 class InterestsController < ApplicationController
   def index
+    @interests = Interest.all
   end
 
   def new
+    @interest = Interest.new
   end
 
   def create
+    @interest = Interest.new(interest_params)
+    if @interest.save 
+      redirect_to interest_path(@interest)
+    else
+      render action: 'new'
+    end
   end
 
   def show
+    set_interest
   end
 
   def edit
+    set_interest
   end
 
   def update
+    set_interest
+
+    if @interest.update(interest_params)
+      redirect_to @interest, notice: 'Interest was successfully updated.'
+    else
+      render action: 'edit'
+    end
+
   end
 
   def destroy
+    if set_interest.destroy
+      redirect_to interests_path, notice: 'Interest deleted.'
+    else
+      render action: 'index'
+    end
   end
+
+  private
+    
+    # Use callbacks to share common setup or constraints between actions.
+    def set_interest
+      @interest = Interest.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def interest_params
+      params.require(:interest).permit([:name])
+    end
+
 end
