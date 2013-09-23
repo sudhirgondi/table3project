2e6316fe3a3eece0122e5735d904915f24e77425
Jesse Lin <jesselin@gmail.com>
2013-09-23 16:45:10 -0700

    remove zip required from create event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 	has_many :posts,:dependent=>:destroy
 	has_many :users, through: :event_attendants
 	belongs_to :interest
-	
+
 	validates :name, :description, :location, :address, presence:true
-	validates :zip_code,  :presence => true, :numericality => { :greater_than_or_equal_to => 10000, :less_than_or_equal_to => 99999, :message => "must be valid." }
+	# validates :zip_code, :numericality => { :greater_than_or_equal_to => 10000, :less_than_or_equal_to => 99999, :message => "must be valid." }
 end
