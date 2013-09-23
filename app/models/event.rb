41f034c4c79bd146a850fcab9a215c3faffa42a9
Jesse Lin <jesselin@gmail.com>
2013-09-23 16:40:52 -0700

    create event changes, edit event changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
-	validates :name, :description, :location, :address, presence:true
 	has_many :posts,:dependent=>:destroy
 	has_many :users, through: :event_attendants
 	belongs_to :interest
+	
+	validates :name, :description, :location, :address, presence:true
+	validates :zip_code,  :presence => true, :numericality => { :greater_than_or_equal_to => 10000, :less_than_or_equal_to => 99999, :message => "must be valid." }
 end
