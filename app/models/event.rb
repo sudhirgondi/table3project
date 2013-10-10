62db9e13399825f60121a36002bc0f98f20f1ce8
Andrew Garcia <tke578@gmail.com>
2013-10-10 12:59:48 -0700

    Fix geocoding for Events created and added geocoding for user registration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,18 @@
 class Event < ActiveRecord::Base
-	validates :name, :description, :location, :address, presence:true
+	validates :name, :description, :location, :address,:city,:state,:zip_code, presence:true
 	has_many :event_attendants
 	has_many :posts,:dependent=>:destroy
 	has_many :users, through: :event_attendants
 	belongs_to :interest
 
-	geocoded_by :address
- 	after_validation :geocode, :if => :address_changed?
+	geocoded_by :location
+ 	after_validation :geocode, :if => :location_changed?
 
  	acts_as_gmappable :process_geocoding => false
+
+ 	def location
+ 		[address,city,state,zip_code].compact.join(',')
+ 	end
 
 	def gmaps4rails_address
 	#describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
